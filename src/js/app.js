de7976a5b87ecd0c4923eb110f53abeca1ce40bf
Ken Suong <kensuong@gmail.com>
2017-06-24 19:49:35 -0400

    Window passenger now adjust pbased on openWeather API

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,6 @@ gm.info.getVehicleConfiguration(function(data) {
   vinElem.innerHTML = gm.info.getVIN();
 });
 
-
 /**************
 DRIVER
 ***************/
@@ -46,4 +45,41 @@ function setRightTemp(data) {
     data.front_right_set_temperature = 68;
     console.log("The temperature for the right is " + data.front_right_set_temperature);
   }
+}
+
+// set windows to weather preferences
+// gm.info.getVehicleData(setPassengerWindow, ['window_passenger']);
+var weatherURL = "http://api.openweathermap.org/data/2.5/weather?q=Toronto&APPID=6fae7452cfc72dfb47f1dbee0ebfc635";
+
+$.getJSON(weatherURL, function(data){
+  console.log(data.weather[0].main);
+  var weatherOutside = data.weather[0].main;
+  if (weatherOutside == "Clouds") {
+    console.log("The weather outside is " + weatherOutside);
+  }
+  if (weatherOutside == "Rain") {
+    console.log("The weather outside is " + weatherOutside); // suppose to print out rain
+    // roooooooollll the windows upppp
+    gm.info.getVehicleData(closePassengerWindow, ['window_passenger']);
+  }
+  if (weatherOutside == "Sunny") {
+    gm.info.getVehicleData(openPassengerWindow, ['window_passenger']);
+  }
+});
+
+function closePassengerWindow(data) {
+  // close the windows
+  console.log("Windows are " + data.window_passenger);
+  // window_passeger is an int. 0 is closed. Set window_passenger to 0 to close
+  data.window_passenger = 0;
+  // do ai to figure out what user preferences are during certain weather patterns
+}
+
+function openPassengerWindow(data) {
+  // open the windows on a good day
+  console.log("Windows are " + data.window_passenger);
+  // window_passenger is an int. 6 is fully open. Set window_passenger to 5 to close
+  data.window_passenger = 6;
+
+  // do ai to figure out what user preferences are during certain weather patterns
 }
