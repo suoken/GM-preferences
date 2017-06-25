0408d4afab01dfaf49ecfedc1c036f94c2242c4f
Ken Suong <kensuong@gmail.com>
2017-06-24 20:50:08 -0400

    Rear passenger close and windows turn on when it rains

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,38 @@ gm.info.getVehicleConfiguration(function(data) {
   vinElem.innerHTML = gm.info.getVIN();
 });
 
+/***********
+BOTH DRIVER AND PASSENGER
+*************/
+
+// set windows to weather preferences
+var weatherURL = "http://api.openweathermap.org/data/2.5/weather?q=Toronto&APPID=6fae7452cfc72dfb47f1dbee0ebfc635";
+
+$.getJSON(weatherURL, function(data){
+  console.log(data.weather[0].main);
+  var weatherOutside = data.weather[0].main;
+  if (weatherOutside == "Clouds") {
+    console.log("The weather outside is " + weatherOutside);
+  }
+  if (weatherOutside == "Rain") {
+    console.log("The weather outside is " + weatherOutside); // suppose to print out rain
+    gm.info.getVehicleData(closePassengerWindow, ['window_passenger']);
+    gm.info.getVehicleData(closeDriverWindow, ['window_driver']);
+    gm.info.getVehicleData(closeRearWindow, ['window_leftrear', 'window_rightrear']);
+
+    // turn the wipers on when it rains
+    gm.info.getVehicleData(wipersOn, ['wipers_on']);
+  }
+  if (weatherOutside == "Sunny") {
+    // I am assuming that because it is sunny outside, they want their windows
+    // open. AI hopefully will learn from their preferences (sometimes they like the windows open or closed)
+    gm.info.getVehicleData(openPassengerWindow, ['window_passenger']);
+    gm.info.getVehicleData(openDriverWindow, ['window_driver']);
+    gm.info.getVehicleData(openRearRightWindow, ['window_rightrear']);
+    gm.info.getVehicleData(openRearLeftWindow, ['window_leftrear']);
+  }
+});
+
 /**************
 DRIVER
 ***************/
@@ -29,7 +61,17 @@ function setLeftTemp(data) {
   }
 }
 
+function closeDriverWindow(data) {
+  // window_driver is an int. 0 is closed. Set window_passenger to 0 to close
+  data.window_driver = 0;
+  console.log("Windows are " + data.window_driver);
+}
 
+function openDriverWindow(data) {
+  // 6 is open windows
+  data.window_driver = 6;
+  console.log("Windows are " + data.window_driver);
+}
 
 /************
 PASSENGER
@@ -47,26 +89,6 @@ function setRightTemp(data) {
   }
 }
 
-// set windows to weather preferences
-// gm.info.getVehicleData(setPassengerWindow, ['window_passenger']);
-var weatherURL = "http://api.openweathermap.org/data/2.5/weather?q=Toronto&APPID=6fae7452cfc72dfb47f1dbee0ebfc635";
-
-$.getJSON(weatherURL, function(data){
-  console.log(data.weather[0].main);
-  var weatherOutside = data.weather[0].main;
-  if (weatherOutside == "Clouds") {
-    console.log("The weather outside is " + weatherOutside);
-  }
-  if (weatherOutside == "Rain") {
-    console.log("The weather outside is " + weatherOutside); // suppose to print out rain
-    // roooooooollll the windows upppp
-    gm.info.getVehicleData(closePassengerWindow, ['window_passenger']);
-  }
-  if (weatherOutside == "Sunny") {
-    gm.info.getVehicleData(openPassengerWindow, ['window_passenger']);
-  }
-});
-
 function closePassengerWindow(data) {
   // close the windows
   console.log("Windows are " + data.window_passenger);
@@ -82,4 +104,28 @@ function openPassengerWindow(data) {
   data.window_passenger = 6;
 
   // do ai to figure out what user preferences are during certain weather patterns
+}
+
+/**************
+REAR PASSENGERS
+**************/
+function closeRearWindow(data) {
+  // 0 is closed. Because it's raining, you obviously want it closed
+  data.window_leftrear = 0;
+  data.window_rightrear = 0;
+}
+
+function  openRearRightWindow(data) {
+  // 6 is open. More AI to learn what the user actually wants ("Does the person want windows partially open...?")
+  data.window_rightrear = 6;
+}
+
+function openRearLeftWindow(data) {
+  // 6 is open. More AI to learn what the user actually wants
+  data.window_leftrear = 6;
+}
+
+function wipersOn(data) {
+  data.wipers_on = 1;
+  console.log("Wipers are on"); 
 }
