a3a1da1392b02f8287a84e617cffc771f97efb49
Pravpro <pravir.adlakha@mail.utoronto.ca>
2017-06-25 11:10:19 -0400

    main menu

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,12 @@ var vinElem = document.getElementById('vin');
 gm.info.getVehicleConfiguration(function(data) {
   vinElem.innerHTML = gm.info.getVIN();
 });
+
+// var weatherLink = document.querySelector("#temp-disp");
+
+$("#temp-disp").on('click', function() {
+	$(".row").fadeOut(function() {
+		location.href="index.html";
+	});
+});
+
