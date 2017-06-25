8f3e8da779924f9b316ea2d2a59e5aa5e096a0b0
Mitul Shah <mitulxshah@gmail.com>
2017-06-24 21:00:48 -0400

    transition for skip

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,11 @@ var vinElem = document.getElementById('vin');
 gm.info.getVehicleConfiguration(function(data) {
   vinElem.innerHTML = gm.info.getVIN();
 });
+
+var skip = document.getElementById('skip');
+var welcome = document.getElementById('welcome');
+var second = document.getElementById('secondary');
+skip.addEventListener('click', function(){
+   welcome.classList.add('hide');
+   second.classList.remove('hide');
+})
